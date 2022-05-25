7a9e6784682f631ba2e2618c5494b91f85c2f561
JoachimSand <joachimliptrotsand@gmail.com>
2022-05-25 11:02:58 +0100

    Added basic structure for SPI interface with optical flow sensor.

---
+++ main/optical_flow_sensor.h
@@ -0,0 +1,14 @@
+#include "platform.h"
+#include "driver/spi_master.h"
+#include "driver/gpio.h"
+#include "portmacro.h"
+
+// use the SPI2 controller for the optical flow sensor.
+// note that there is only one other SPI
+#define OPT_FLOW_HOST SPI2_HOST
+
+static const char TAG[] = "OptFlowSensor";
+
+void transmit(spi_device_handle_t *spi_handle);
+
+void init_opt_flow_sensor(u8 PIN_MISO, u8 PIN_MOSI, u8 PIN_CLK, u8 PIN_CS);
